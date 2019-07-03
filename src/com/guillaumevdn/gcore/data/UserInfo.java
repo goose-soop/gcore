2532a37a7c8e3e23c8554d44274fc1fe68910228
Guillaume Vandenneucker <vandenneucker.guillaume@gmail.com>
2019-07-03 22:46:35 +0200

    Inverted a line ?? x)

--- src/com/guillaumevdn/gcore/data/UserInfo.java
+++ src/com/guillaumevdn/gcore/data/UserInfo.java
@@ -79,8 +79,8 @@ public class UserInfo {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((profile == null) ? 0 : profile.hashCode());
 		result = prime * result + ((uuid == null) ? 0 : uuid.hashCode());
+		result = prime * result + ((profile == null) ? 0 : profile.hashCode());
 		return result;
 	}
 
